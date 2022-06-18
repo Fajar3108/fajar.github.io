1055d8d3b667b770782e7b6e7dee9898fdedfd00
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-06-18 11:18:17 +0700

    Update: change padding for footer

--- src/pages/Home/partials/Footer.js
+++ src/pages/Home/partials/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     const date = new Date().getFullYear();
     return (
-        <footer className="w-full text-center py-12">
+        <footer className="w-full text-center py-8">
             <p>Copyright {date} &copy; <a href="https://www.instagram.com/Fajar3108.i" target="_blank" rel="noreferrer" className="text-blue-600">Maulana Fajar Ibrahim</a></p>
         </footer>
     )
