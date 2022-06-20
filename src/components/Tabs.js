289abd7a2fac3eb302b9e39c28a4375c750d0d31
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-06-20 12:53:54 +0700

    Add: Active Link Tab

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,12 +1,12 @@
-import { Link  } from "react-router-dom";
+import { NavLink  } from "react-router-dom";
+import '../assets/css/Tab.css';
 
 const Tabs = () => {
-
     return (
-        <div className="grid md:grid-cols-3 grid-cols-1 mb-3">
-            <Link to="/" className="py-2 text-center border text-sm">Works</Link>
-            <Link to="/experiences" className="py-2 text-center border text-sm">Experiences</Link>
-            <Link to="/archivements" className="py-2 text-center border text-sm">Archivements</Link>
+        <div className="grid md:grid-cols-3 grid-cols-1 mb-3 text-center text-sm">
+            <NavLink to="/" className="py-2 border">Works</NavLink>
+            <NavLink to="/experiences" className="py-2 border">Experiences</NavLink>
+            <NavLink to="/archivements" className="py-2 border">Archivements</NavLink>
         </div>
     )
 }
