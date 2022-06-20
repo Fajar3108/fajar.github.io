04e9f9ae6aa918df97c32fd785032dc28a193aea
Fajar3108 <maulanafajaribrahim@gmail.com>
2022-06-20 09:59:13 +0700

    Update: Experience Card

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -8,6 +8,9 @@ const Works = () => {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget dui platea amet congue faucibus in euismod. Quis in nunc, imperdiet pellentesque.',
             download: '',
             view: '',
+            tags: [
+                'React JS', 'Laravel', 'Bootstrap'
+            ]
         },
         {
             image: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400/da586d71391479.5bc43cb04c5b3.jpg',
@@ -15,6 +18,9 @@ const Works = () => {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget dui platea amet congue faucibus in euismod. Quis in nunc, imperdiet pellentesque.',
             download: '',
             view: '',
+            tags: [
+                'React JS', 'Laravel', 'Bootstrap'
+            ]
         },
         {
             image: 'https://www.talentvis.com/files/images/blog/2021/09/6758222e_Beginners-Guide-to-New-Skill-UI-design.jpg',
@@ -22,6 +28,9 @@ const Works = () => {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget dui platea amet congue faucibus in euismod. Quis in nunc, imperdiet pellentesque.',
             download: '',
             view: '',
+            tags: [
+                'React JS', 'Laravel', 'Bootstrap'
+            ]
         },
         {
             image: 'https://i.pinimg.com/736x/cf/d0/0f/cfd00ff613363792d38fb666df03331e.jpg',
@@ -29,6 +38,9 @@ const Works = () => {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget dui platea amet congue faucibus in euismod. Quis in nunc, imperdiet pellentesque.',
             download: '',
             view: '',
+            tags: [
+                'React JS', 'Laravel', 'Bootstrap'
+            ]
         },
     ]
 
@@ -38,8 +50,15 @@ const Works = () => {
                 works.map((work) => (
                 <div className="w-full bg-gray-400 relative overflow-hidden group mb-3" key={nanoid()}>
                     <img src={work.image} alt=" " className="w-full object-cover" />
-                    <div className="p-4 absolute bg-black/50 z-50 -bottom-72 group-hover:bottom-0 transition-all text-white">
+                    <div className="p-4 absolute bg-black/70 z-50 -bottom-full group-hover:bottom-0 transition-all text-white">
                         <h2 className="text-xl mb-1">{work.title}</h2>
+                        <div className="text-blue-600">
+                            {
+                                work.tags.map((tag) => (
+                                    <span className="mr-2" key={nanoid()}>#{tag}</span>
+                                ))
+                            }
+                        </div>
                         <p className="text-sm text-gray-200">{work.description}</p>
                         <div className="flex">
                             <a className="px-12 py-2 text-sm mt-3 cursor-pointer bg-transparent border text-white hover:bg-blue-600 hover:text-white transition-all" href={work.view}>View</a>
